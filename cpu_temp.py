e4837b552e8066f02e325f8816718af426b8da15
ckirkwood <callum.kirkwood@gmail.com>
2017-07-10 21:38:47 +0000

    cpu temp monitor test - 8x8

---
+++ cpu_temp.py
@@ -0,0 +1,13 @@
+from UHScroll import *
+from time import sleep
+import os 
+
+# Return CPU temperature as a character string                                      
+def getCPUtemperature():
+    res = os.popen('vcgencmd measure_temp').readline()
+    return(res.replace("temp=","").replace("'C\n",""))
+
+temp = getCPUtemperature()
+
+unicorn_scroll(temp + '~degrsc','yellow',200,0.4)
+
