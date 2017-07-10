772131c7727b35d4372049f5e1d7b77aa67e3286
ckirkwood <callum.kirkwood@gmail.com>
2017-07-10 21:59:55 +0000

    speed up scrolling

--- cpu_temp.py
+++ cpu_temp.py
@@ -2,12 +2,11 @@ from UHScroll import *
 from time import sleep
 import os 
 
-# Return CPU temperature as a character string                                      
 def getCPUtemperature():
     res = os.popen('vcgencmd measure_temp').readline()
     return(res.replace("temp=","").replace("'C\n",""))
 
 temp = getCPUtemperature()
 
-unicorn_scroll(temp + '~degrsc','yellow',200,0.1)
+unicorn_scroll('CPU-' + temp + '~degrsc','yellow',200,0.07)
 
